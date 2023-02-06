e3a2ab27f30e7107d6a74128816499f73f3400e3
Gonzalo De Domingo <gonzalodedomingo@gmail.com>
2023-02-06 14:40:49 -0300

    Add front code

---
+++ front/src/App.svelte
@@ -0,0 +1,156 @@
+<script>
+  import { Alert, Card, Button, Textarea, Spinner, P } from "flowbite-svelte";
+  import { DateInput } from "date-picker-svelte";
+  import { onMount } from "svelte";
+
+  import TimeLineComponent from "./components/Timeline.svelte";
+  import FloatButton from "./components/FloatButton.svelte";
+  import EmojiStatus from "./components/EmojiStatus.svelte";
+
+  const API_URL = import.meta.env.VITE_API_URL;
+  let text;
+  let loading = false;
+  let responses = [];
+  let date = new Date();
+  let today = new Date();
+  let userStatus = { happiness: 0, sadness: 0 };
+
+  $: {
+    userStatus = calculateUserStatus(responses);
+  }
+
+  const onSubmit = async () => {
+    try {
+      loading = true;
+      const response = await fetch(`${API_URL}/post`, {
+        method: "POST",
+        body: JSON.stringify({ text, userPreferences: addKnowToApp() }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!response.ok) throw new Error("WARN", response?.status);
+      const data = await response.json();
+
+      responses = [
+        ...responses,
+        { text, result: data, date: date.toISOString() },
+      ];
+
+      localStorage.setItem("userData", JSON.stringify(responses));
+      text = "";
+      return data;
+    } catch (error) {
+      console.log(error);
+    } finally {
+      loading = false;
+    }
+  };
+
+  const addKnowToApp = () =>
+    responses.map((userResponse) => ({
+      text: userResponse?.text,
+      label: userResponse?.result?.result,
+    }));
+
+  const clearUserData = () => {
+    localStorage.removeItem("userData");
+    responses = [];
+  };
+
+  const calculateUserStatus = (responses) => {
+    if (responses.length === 0) return;
+    let happinessCounter = 0;
+    let happinessAcumulator = 0;
+    let sadnessCounter = 0;
+    let sadnessAcumulator = 0;
+    let totalCounter = 0;
+
+    try {
+      responses.forEach((response) => {
+        totalCounter += 1;
+        if (response?.result?.result === "negative") {
+          sadnessCounter++;
+          sadnessAcumulator += response?.result?.confidence;
+        } else {
+          happinessCounter++;
+          happinessAcumulator += response?.result?.confidence;
+        }
+      });
+
+      return {
+        happiness: (happinessAcumulator / totalCounter) * 100,
+        sadness:
+          sadnessCounter > 0 ? (sadnessAcumulator / totalCounter) * 100 : 0,
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  onMount(async () => {
+    try {
+      let userData = localStorage.getItem("userData");
+      if (userData) {
+        userData = JSON.parse(userData);
+        responses = userData || [];
+      } else {
+        localStorage.userData = JSON.stringify([]);
+      }
+    } catch (error) {
+      console.log("Error on load user data");
+    }
+  });
+</script>
+
+<div class="containerMain">
+  <Card rounded style="max-width: 60vw; width: 50vw;">
+    <div class="flex justify-center flex-col">
+      <div class="text-7xl">😎</div>
+    </div>
+    <div class="mb-4 mt-8">
+      <Alert>
+        <P size="base">Hello! Welcome to AI Personal Diary</P>
+        <P size="sm"
+          >You can add entries to the diary and the AI will assess your weekly
+          mood 😊</P
+        >
+      </Alert>
+    </div>
+    <EmojiStatus {userStatus} />
+    <div class="mb-5 flex justify-start items-center">
+      <P class="mr-3">Date:</P>
+      <DateInput bind:value={date} format="dd-MM-yyyy" max={today} />
+    </div>
+    <div class="mb-5">
+      <Textarea
+        placeholder="Write a brief description of your day..."
+        bind:value={text}
+        disabled={loading}
+        rows="4"
+        style="resize: none;"
+      />
+    </div>
+    <Button
+      type="submit"
+      class="w-full"
+      on:click={onSubmit}
+      disabled={loading || text?.length < 10}
+    >
+      {#if loading}<Spinner class="mr-3" size="4" color="white" />{/if}
+      Send
+    </Button>
+  </Card>
+  <TimeLineComponent {responses} />
+  <FloatButton on:reset={clearUserData} />
+</div>
+
+<style>
+  .containerMain {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    margin-top: 10px;
+  }
+</style>
---
+++ front/src/components/FloatButton.svelte
@@ -0,0 +1,53 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  import { SpeedDial, SpeedDialButton } from "flowbite-svelte";
+
+  const moveToTop = () => window?.scrollTo({ top: 0, behavior: "smooth" });
+
+  const dispatch = createEventDispatcher();
+  const reset = () => dispatch("reset");
+</script>
+
+<SpeedDial defaultClass="fixed right-6 bottom-6">
+  <svg
+    slot="icon"
+    aria-hidden="true"
+    class="w-8 h-8"
+    fill="currentColor"
+    viewBox="0 0 20 20"
+    xmlns="http://www.w3.org/2000/svg"
+    ><path
+      d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"
+    /></svg
+  >
+  <SpeedDialButton name="Reset" on:click={reset}>
+    <svg viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg"
+      ><g id="SVGRepo_bgCarrier" stroke-width="0" /><g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      /><g id="SVGRepo_iconCarrier">
+        <path
+          d="M4.56189 13.5L4.14285 13.9294L4.5724 14.3486L4.99144 13.9189L4.56189 13.5ZM9.92427 15.9243L15.9243 9.92427L15.0757 9.07574L9.07574 15.0757L9.92427 15.9243ZM9.07574 9.92426L15.0757 15.9243L15.9243 15.0757L9.92426 9.07574L9.07574 9.92426ZM19.9 12.5C19.9 16.5869 16.5869 19.9 12.5 19.9V21.1C17.2496 21.1 21.1 17.2496 21.1 12.5H19.9ZM5.1 12.5C5.1 8.41309 8.41309 5.1 12.5 5.1V3.9C7.75035 3.9 3.9 7.75035 3.9 12.5H5.1ZM12.5 5.1C16.5869 5.1 19.9 8.41309 19.9 12.5H21.1C21.1 7.75035 17.2496 3.9 12.5 3.9V5.1ZM5.15728 13.4258C5.1195 13.1227 5.1 12.8138 5.1 12.5H3.9C3.9 12.8635 3.92259 13.2221 3.9665 13.5742L5.15728 13.4258ZM12.5 19.9C9.9571 19.9 7.71347 18.6179 6.38048 16.6621L5.38888 17.3379C6.93584 19.6076 9.54355 21.1 12.5 21.1V19.9ZM4.99144 13.9189L7.42955 11.4189L6.57045 10.5811L4.13235 13.0811L4.99144 13.9189ZM4.98094 13.0706L2.41905 10.5706L1.58095 11.4294L4.14285 13.9294L4.98094 13.0706Z"
+          fill="currentColor"
+        />
+      </g></svg
+    >
+  </SpeedDialButton>
+  <SpeedDialButton name="To top" on:click={moveToTop}>
+    <svg
+      fill="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      ><g id="SVGRepo_bgCarrier" stroke-width="0" /><g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      /><g id="SVGRepo_iconCarrier">
+        <path
+          d="M13,5.70710678 L13,16.5 C13,16.7761424 12.7761424,17 12.5,17 C12.2238576,17 12,16.7761424 12,16.5 L12,5.70710678 L8.85355339,8.85355339 C8.65829124,9.04881554 8.34170876,9.04881554 8.14644661,8.85355339 C7.95118446,8.65829124 7.95118446,8.34170876 8.14644661,8.14644661 L12.1464466,4.14644661 C12.3417088,3.95118446 12.6582912,3.95118446 12.8535534,4.14644661 L16.8535534,8.14644661 C17.0488155,8.34170876 17.0488155,8.65829124 16.8535534,8.85355339 C16.6582912,9.04881554 16.3417088,9.04881554 16.1464466,8.85355339 L13,5.70710678 Z M4.5,20 C4.22385763,20 4,19.7761424 4,19.5 C4,19.2238576 4.22385763,19 4.5,19 L20.5,19 C20.7761424,19 21,19.2238576 21,19.5 C21,19.7761424 20.7761424,20 20.5,20 L4.5,20 Z"
+        />
+      </g></svg
+    >
+  </SpeedDialButton>
+</SpeedDial>
