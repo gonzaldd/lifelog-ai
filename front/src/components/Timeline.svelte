d82b0bd7c12f851dc1af52c8480074e683fab009
Gonza <gdedomingo>
2023-02-06 14:40:49 -0300

    Add front code

---
+++ front/src/components/Timeline.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { Card, P, Timeline, TimelineItem } from "flowbite-svelte";
+
+  export let responses;
+</script>
+
+{#if responses?.length > 0}
+  <Card class="mt-5 text-left" style="max-width: 60vw; width: 50vw;">
+    <Timeline>
+      {#await responses then}
+        {#each responses as response}
+          <TimelineItem date={new Date(response?.date).toDateString()}>
+            <p
+              class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400"
+            >
+              {response?.text}
+            </p>
+            <P
+              color={response?.result?.result === "negative"
+                ? "text-red-700"
+                : "text-green-700"}
+              >{response?.result?.result}
+              {Math.trunc(response?.result?.confidence * 100)}%</P
+            >
+          </TimelineItem>
+        {/each}
+      {/await}
+    </Timeline>
+  </Card>
+{/if}
